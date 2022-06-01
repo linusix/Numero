a102c0c5fad06c3d51feb806ba340b4f77bdc846
linusix <linusix@gmail.com>
2022-06-01 14:44:36 +0900

    a

--- Numero/AppDelegate.swift
+++ Numero/AppDelegate.swift
@@ -9,7 +9,7 @@
 ///
 /// The above copyright notice and this permission notice shall be included in
 /// all copies or substantial portions of the Software.
-///
+///asdasd
 /// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
 /// distribute, sublicense, create a derivative work, and/or sell copies of the
 /// Software in any work that is designed, intended, or marketed for pedagogical or
